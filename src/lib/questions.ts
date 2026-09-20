48e034473091e91b88b8990463e0315746cce0c8
Khanh Thinh Nguyen <1468695+thinhnk55@users.noreply.github.com>
2026-09-21 00:32:26 +0700

    content: improve quiz explanations and sources

--- src/lib/questions.ts
+++ src/lib/questions.ts
@@ -1,4 +1,5 @@
 import rawQuestions from '../data/van-mieu-quoc-tu-giam.questions.json'
+import { getExplanation } from './explanation-notes'
 
 export interface Question {
   id: number
@@ -14,6 +15,7 @@ export interface Question {
 export const questions = (rawQuestions as Question[]).map((question, index) => ({
   ...question,
   id: index + 1,
+  explanation: getExplanation(index + 1, question.explanation),
 }))
 export const questionIds = questions.map((question) => question.id)
 
